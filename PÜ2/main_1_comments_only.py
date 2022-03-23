7ef0585fae4bcdfe659ae0a7ed3ca611ba18c9a3
lillinml <nl4090@mci4me.at>
2022-03-23 11:00:17 +0100

    UC comments

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,4 +1,24 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
+# Dateien öffnen und mit numpy array konvertieren
+# 
+
+#%% UC 2.2 Vorverarbeiten der Daten
+# 
+
+#%% UC 2.3 Analysen der Daten auf Abbruchkriterium
+# 
+
+#%% UC 2.4 Erstellen einer Zusammenfassung
+# 
+
+#%% UC 2.5 Visualisierung der Daten
+# 
+
+#%% UC 2.6 Manuelle Eingabe eines Abbruchkriteriums
+# 
+
+#%% UC 2.7 Speichern der Daten
+# 
 

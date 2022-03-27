073734fa94d0f2f3a835787be47c67c151ef5e15
lillinml <nl4090@mci4me.at>
2022-03-27 20:25:49 +0200

    Update

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -5,20 +5,23 @@
 # 
 
 #%% UC 2.2 Vorverarbeiten der Daten
-# 
+# Fehler beseitigen
+
 
 #%% UC 2.3 Analysen der Daten auf Abbruchkriterium
-# 
+# wenn Abbruchkriterium erreicht wurde, Daten aussortieren
 
 #%% UC 2.4 Erstellen einer Zusammenfassung 
-#
+# Herausgefilterte Daten zusammenfassen
+# 
 
 #%% UC 2.5 Visualisierung der Daten
-# 
+#  einen Graphen erstellen mit matplotlib oder eine Textdatei
 
 #%% UC 2.6 Manuelle Eingabe eines Abbruchkriteriums
-# 
+# individuell bei Ausnahmen die Daten heraussortieren
 
 #%% UC 2.7 Speichern der Daten
-# 
+#  Sicherung der fertigen Daten
+# An Auftraggeber/Kunde weitergeben
 

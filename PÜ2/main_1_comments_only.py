b9b35a9e8c1d7db1834159c30217f1cfd9e62c02
Lenerjo <101863861+Lenerjo@users.noreply.github.com>
2022-03-29 11:23:51 +0200

    update

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -2,7 +2,7 @@
 
 # UC 2.1 Einlesen der Daten
 # Dateien öffnen und mit numpy array konvertieren
-# 
+# Eingabe Dateien sind aufgrund der letzten leeren Zeile #181 ein String Datentyp. Dieser Eintrag muss gelöscht werden
 
 # UC 2.2 Vorverarbeiten der Daten
 # Fehler beseitigen
@@ -11,13 +11,15 @@
 
 # UC 2.3 Analysen der Daten auf Abbruchkriterium
 # wenn Abbruchkriterium erreicht wurde, Daten aussortieren
+# Falsche Daten seperat abspeichern
 
 # UC 2.4 Erstellen einer Zusammenfassung 
 # Herausgefilterte Daten zusammenfassen
 # Einen Überblick erstellen, welche Werte gefunden und benutzt wurden
 
 # UC 2.5 Visualisierung der Daten
-#  einen Graphen mit matplotlib oder eine Textdatei erstellen
+# einen Graphen mit matplotlib oder eine Textdatei erstellen
+# Visualisierung variabel gestalten
 
 # UC 2.6 Manuelle Eingabe eines Abbruchkriteriums
 # Individuell bei Ausnahmen die Daten heraussortieren

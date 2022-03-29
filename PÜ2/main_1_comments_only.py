7f523823038b70780eb0d26b7e970815c588b81e
Lenerjo <101863861+Lenerjo@users.noreply.github.com>
2022-03-29 19:08:43 +0200

    finished

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -2,20 +2,22 @@
 
 # UC 2.1 Einlesen der Daten
 # Dateien öffnen und mit numpy array konvertieren
-# Eingabe Dateien sind aufgrund der letzten leeren Zeile #181 ein String Datentyp. Dieser Eintrag muss gelöscht werden
+# Eingabe Dateien sind aufgrund der letzten leeren Zeile #181 ein String Datentyp. Dieser leere Eintrag muss gelöscht werden
 
 # UC 2.2 Vorverarbeiten der Daten
+# Daten werden auf Plausibilität überürüft
 # Fehler beseitigen
-# Entscheiden, welche Werte von Bedeutung sind
-
+# Aussortierung von Messwerten die auf einen Messfehler hindeuten (zu große Abweichung von Mittelwert)
+# Richtige Daten als neues Array anlegen
 
 # UC 2.3 Analysen der Daten auf Abbruchkriterium
 # wenn Abbruchkriterium erreicht wurde, Daten aussortieren
-# Falsche Daten seperat abspeichern
+# Falsche Daten seperat abspeichern um wahre und falsche Daten zu unterscheiden
 
 # UC 2.4 Erstellen einer Zusammenfassung 
 # Herausgefilterte Daten zusammenfassen
 # Einen Überblick erstellen, welche Werte gefunden und benutzt wurden
+# Visualisierung um Datenstand einfach abzulesen
 
 # UC 2.5 Visualisierung der Daten
 # einen Graphen mit matplotlib oder eine Textdatei erstellen
